52417305d7837b60a3f38161d74182392de8188a
Vladimir Dorofeev <daskodex@gmail.com>
2023-03-30 14:29:30 +0600

    project re init

---
+++ pages/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+# №	заказ №	стоимость,$	срок поставки
+# Create your models here.
+class ParseResult(models.Model):
+    number = models.PositiveIntegerField(default=0,
+                                         verbose_name='Порядковый номер'
+                                         )
+
+    order_id = models.PositiveIntegerField(default=0,
+                                           verbose_name='Заказ №',
+                                           )
+
+    price = models.PositiveIntegerField(default=0,
+                              verbose_name='Cтоимость,$',
+                              )
+
+    delivery_time = models.DateField(verbose_name='Срок поставки',
+                                     )
+
+    delivery_time_orig = models.CharField(max_length=20,
+                                          verbose_name='Срок поставки (исх.дата)',
+                                          default=''
+                                          )
+
+    created_at = models.DateTimeField(verbose_name='Время получения данных',
+                                      auto_now=True,
+                                      )
