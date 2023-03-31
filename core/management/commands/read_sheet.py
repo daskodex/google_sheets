ffadc5cbad4266c9763323c25331c8fbf91a6801
Vladimir Dorofeev <daskodex@gmail.com>
2023-03-31 15:46:01 +0600

    structure refactoring

--- core/management/commands/read_sheet.py
+++ core/management/commands/read_sheet.py
@@ -1,64 +1,13 @@
 from django.core.management.base import BaseCommand
-import httplib2
-import apiclient.discovery
-from oauth2client.service_account import ServiceAccountCredentials
-from pages.models import ParseResult
-from pages.tools import get_course, DrowGraph, SendTGMessage
-from datetime import datetime, date
-from decouple import config
+
+from pages.tools import DrowGraph, SendTGMessage, ReadSheets
+
 
 class Command(BaseCommand):
     help = 'Google Sheets Reader'
 
     def handle(self, *args, **options):
-
-        CREDENTIALS_FILE = config('CREDENTIALS_FILE')
-        SPREADSHEETS_ID = config('SPREADSHEETS_ID')
-
-        # Авторизуемся и получаем service — экземпляр доступа к API
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(
-            CREDENTIALS_FILE,
-            ['https://www.googleapis.com/auth/spreadsheets',
-             'https://www.googleapis.com/auth/drive'])
-        httpAuth = credentials.authorize(httplib2.Http())
-        service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
-
-        # Пример чтения файла
-        values = service.spreadsheets().values().get(
-            spreadsheetId=SPREADSHEETS_ID,
-            range='A1:D60',
-            majorDimension='COLUMNS'
-        ).execute()
-
-        #TODO поменять на динамическое определение диапазона
-
-        ParseResult.objects.all().delete()
-
-        expired_log = ''
-
-        for i in range(1, len(values['values'][0])):
-
-            parsed_price = int(float(values['values'][2][i]) * float(get_course()))
-
-            d = datetime.strptime(values['values'][3][i], "%d.%m.%Y")
-            parsed_date = d.strftime('%Y-%m-%d')
-
-            expired = True if str(date.today()) > parsed_date else False
-
-            #Функция шлет сообщения от бота о том что заказ просрочен
-
-            ParseResult(number=values['values'][0][i],
-                        order_id=values['values'][1][i],
-                        price=parsed_price,
-                        delivery_time=parsed_date,
-                        delivery_time_orig=values['values'][3][i],
-                        expired=expired,
-                        ).save()
-
-            if expired:
-                expired_log += f'Заказ с ID:{values["values"][1][i]} просрочен\n'
-
+        expired_log = ReadSheets()
         DrowGraph()
-
         SendTGMessage(expired_log)
 
--- pages/tools.py
+++ pages/tools.py
@@ -3,6 +3,12 @@ from .models import ParseResult
 import matplotlib.pyplot as plt
 import telegram
 from decouple import config
+import httplib2
+import apiclient.discovery
+from oauth2client.service_account import ServiceAccountCredentials
+from pages.models import ParseResult
+from datetime import datetime, date
+
 
 
 def get_course():
@@ -67,3 +73,54 @@ def SendTGMessage(msg):
     except telegram.error.BadRequest:
         print('Ошибка: telegram.error.BadRequest:, проверьте ID чата в телеграм')
         return False
+
+
+def ReadSheets():
+    CREDENTIALS_FILE = config('CREDENTIALS_FILE')
+    SPREADSHEETS_ID = config('SPREADSHEETS_ID')
+
+    # Авторизуемся и получаем service — экземпляр доступа к API
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        CREDENTIALS_FILE,
+        ['https://www.googleapis.com/auth/spreadsheets',
+         'https://www.googleapis.com/auth/drive'])
+    httpAuth = credentials.authorize(httplib2.Http())
+    service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
+
+    # Пример чтения файла
+    values = service.spreadsheets().values().get(
+        spreadsheetId=SPREADSHEETS_ID,
+        range='A1:D60',
+        majorDimension='COLUMNS'
+    ).execute()
+
+    # TODO поменять на динамическое определение диапазона
+
+    ParseResult.objects.all().delete()
+
+    expired_log = ''
+
+    for i in range(1, len(values['values'][0])):
+
+        parsed_price = int(float(values['values'][2][i]) * float(get_course()))
+
+        d = datetime.strptime(values['values'][3][i], "%d.%m.%Y")
+        parsed_date = d.strftime('%Y-%m-%d')
+
+        expired = True if str(date.today()) > parsed_date else False
+
+        # Функция шлет сообщения от бота о том что заказ просрочен
+
+        ParseResult(number=values['values'][0][i],
+                    order_id=values['values'][1][i],
+                    price=parsed_price,
+                    delivery_time=parsed_date,
+                    delivery_time_orig=values['values'][3][i],
+                    expired=expired,
+                    ).save()
+
+        if expired:
+            expired_log += f'Заказ с ID:{values["values"][1][i]} просрочен\n'
+
+
+    return expired_log
